b18b3f5c70d1853aa6a020f750bed1e8bbc63e85
vignesh-kalimuthu <vikikalimuthu2001@gmail.com>
2025-07-02 23:45:38 +0530

    DB connection

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
